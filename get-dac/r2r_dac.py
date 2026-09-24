dac0e9eb975675d4bef64b01f1b91b607560d376
Шеховцов Василий <shekhovtsov.vm@phystech.edu>
2026-09-24 12:36:00 +0300

    Сделан в виде класса

--- get-dac/r2r_dac.py
+++ get-dac/r2r_dac.py
@@ -9,25 +9,25 @@ class R2R_DAC:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
 
-        def deinit(self):
-            GPIO.output(self.gpio_bits, 0)
-            GPIO.cleanup()
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
 
-        def set_number(self, number):
-            if not (0 <= number <= 255):
-                print(f"Число {number} вне диапазона 0-255. Установим 0.")
-                number = 0
-            binary = [int(b) for b in bin(number)[2:].zfill(8)]
-            GPIO.output(self.gpio_bits, binary)
+    def set_number(self, number):
+        if not (0 <= number <= 255):
+            print(f"Число {number} вне диапазона 0-255. Установим 0.")
+            number = 0
+        binary = [int(b) for b in bin(number)[2:].zfill(8)]
+        GPIO.output(self.gpio_bits, binary)
         
-        def set_voltage(self, voltage):
-            if not (0.0 <= voltage <= self.dynamic_range):
-                print("Напряжение выходит за дин. диапазон ЦАП")
-                print("Установим 0.0")
-                self.set_number(0)
-                return 
-            number = int(voltage / self.dynamic_range * 255)
-            self.set_number(number)
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print("Напряжение выходит за дин. диапазон ЦАП")
+            print("Установим 0.0")
+            self.set_number(0)
+            return 
+        number = int(voltage / self.dynamic_range * 255)
+        self.set_number(number)
 
 
 if __name__ == "__main__":
